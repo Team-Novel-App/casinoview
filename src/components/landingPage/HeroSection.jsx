fc97cce35cbce005f93613cceae003e155201790
Bibek117 <bibekangdembay@gmail.com>
2025-02-03 23:33:05 +0545

    added fonts and hover

--- src/components/landingPage/HeroSection.jsx
+++ src/components/landingPage/HeroSection.jsx
@@ -5,22 +5,22 @@ import {useRouter} from 'next/navigation';
 const heroSlides = [
   {
     image: "https://images.unsplash.com/photo-1509478861672-91e9a2f90c04?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNhc2lub3xlbnwwfHwwfHx8MA%3D%3D",
-    title: "Level Up Your Gaming Experience",
+    title: "20 % off at pre-registration",
     subtitle: "Join the ultimate gaming community where legends are born. Compete in tournaments, connect with fellow gamers, and embrace victory."
   },
   {
-    image: "https://images.unsplash.com/photo-1511193311914-0346f16efe90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y2FzaW5vfGVufDB8fDB8fHww",
-    title: "Compete at the Highest Level",
+    image: "https://media.istockphoto.com/id/883608066/photo/rows-of-casino-slot-machines.jpg?s=2048x2048&w=is&k=20&c=R8DG5k1m-TYEtJN17un9JpYBy_jCYQE3AoPPO4geGl0=",
+    title: "Spin the wheel of your success",
     subtitle: "Enter tournaments with massive prize pools and prove your worth against the best players worldwide."
   },
   {
     image: "https://images.unsplash.com/photo-1491644386567-d98960637850?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    title: "Join Our Gaming Community",
+    title: "Play Roulette and Win Big",
     subtitle: "Connect with millions of players, share your achievements, and make lasting friendships in our vibrant community."
   },
   {
-    image: "https://images.unsplash.com/photo-1626775238053-4315516eedc9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2FzaW5vfGVufDB8fDB8fHww",
-    title: "Join Our Gaming ",
+    image: "https://media.istockphoto.com/id/1197737664/photo/slot-machine-rolling-drums.jpg?s=2048x2048&w=is&k=20&c=reSfMhLkD_iDe-tkVj72T_yi_z6w4auba8EXlyOM30I=",
+    title: "Play Poker and Win Big",
     subtitle: "Connect with millions of players, share your achievements, and make lasting friendships in our vibrant community."
   }
 ];
@@ -83,18 +83,15 @@ export default function HeroSection() {
 
       <div className="relative z-20 h-full container mx-auto px-4 flex items-center">
         <div className="max-w-3xl animate-fade-in">
-          <h1 className="text-5xl md:text-7xl bg-amber-200 font-retro-signed leading-tight mb-6 animate-slide-up">
-            {heroSlides[currentSlide].title.split(' ').map((word, i) => (
+          <h1 className="text-5xl md:text-7xl font-retro-signed leading-tight mb-6 animate-slide-up">
               <span
-                key={i}
-                className="inline-block animate-word-slide-up bg-clip-text bg-gradient-to-r from-[#4f46e5] via-[#7c3aed] to-[#db2777] text-transparent"
-                style={{ animationDelay: `${i * 0.1}s` }}
+                className="inline-block animate-word-slide-up bg-clip-text bg-gradient-to-r from-[#fcb51d] to-[#f72819] text-transparent"
+                style={{ animationDelay: 0.5 }}
               >
-                {word}{' '}
+                {heroSlides[currentSlide].title}
               </span>
-            ))}
           </h1>
-          <p className="text-gray-300 text-lg mb-8 max-w-xl animate-fade-slide-up">
+          <p className="text-lg mb-8 max-w-xl bg-clip-text bg-gradient-to-r from-[#fcb51d] to-[#f72819] text-transparent animate-fade-slide-up">
             {heroSlides[currentSlide].subtitle}
           </p>
           <div className="flex flex-col sm:flex-row gap-4 animate-fade-slide-up-late">
