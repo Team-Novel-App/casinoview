4b09fad0b05c9d82857609082d7701c4e5a6944f
Maneek-afk <manikttmg11@gmail.com>
2025-02-04 23:11:21 +0545

    featuredSection deleted

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,7 @@
 import React, { useState, useEffect, Suspense } from 'react';
 import Navigation from '@/components/landingPage/Navigation';
 import HeroSection from '@/components/landingPage/HeroSection';
-import Footer from '@/components/landingPage/Footer';
-import ChatPopup from '@/components/landingPage/ChatPopup';
 
-const FeaturesSection = React.lazy(() => import('@/components/landingPage/FeatureSection'));
 const GamesSection = React.lazy(() => import('@/components/landingPage/GamesSection'));
 const EventsSection = React.lazy(() => import('@/components/landingPage/EventsSection'));
 const RulesSection = React.lazy(() => import('@/components/landingPage/RulesSection'));
@@ -13,9 +10,19 @@ const WhyChooseUS = React.lazy(() => import('@/components/landingPage/WhyChooseU
 const FAQSection = React.lazy(() => import('@/components/landingPage/FAQSection'));
 const MarqueeText = React.lazy(() => import('@/components/landingPage/MarqueeText'));
 const AboutUs = React.lazy(() => import('@/components/landingPage/AboutUs'));
+const Footer = React.lazy(() => import('@/components/landingPage/Footer'));
+const ChatPopup = React.lazy(() => import('@/components/landingPage/ChatPopup'));
 
 function Home() {
   const [isScrolled, setIsScrolled] = useState(false);
+
+  const Preloader = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+    </div>
+  );
+};
 
   useEffect(() => {
     const handleScroll = () => {
@@ -28,19 +35,14 @@ function Home() {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white font-sans">
       <Navigation isScrolled={isScrolled} />
-      <HeroSection />
-
-      <Suspense fallback={<div>Loading...</div>}>
-        <FeaturesSection />
-        <GamesSection />
-        <AboutUs />
-        <EventsSection />
-        <MarqueeText />
-        <RulesSection />
-        <WhyChooseUS />
-        <FAQSection />
-      </Suspense>
-
+      <HeroSection />    
+      <GamesSection />
+      <AboutUs />
+      <EventsSection />
+      <MarqueeText />
+      <RulesSection />
+      <WhyChooseUS />
+      <FAQSection />
       <Footer />
       <ChatPopup />
     </div>
